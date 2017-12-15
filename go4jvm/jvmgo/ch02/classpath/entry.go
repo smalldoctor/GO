43f0f54c0fd726aa544fd8c253540062d0156084
thinkings <15312408287@163.com>
2017-12-15 21:58:37 +0800

    jvm&go学习

---
+++ go4jvm/jvmgo/ch02/classpath/entry.go
@@ -0,0 +1,16 @@
+package classpath
+
+import "os"
+
+const pathListSeparator = string(os.PathListSeparator)
+
+type Entry interface {
+	/*
+	className是class的全限定名(因为包路径+类名就是class的相对路径名)，
+	相对路径，文件以.class结尾;以 / 分割;
+	Entry是指向具体的class;
+	*/
+	readClass(className string) ([] byte, Entry, error)
+	// 相当于Java中的toString方法
+	String() string
+}
